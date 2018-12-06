2b272324101cfc86563aee68dbea8e49642c4ee6
Abby Fleming <abby.fleming@provisionsgroup.com>
2018-12-05 19:55:13 -0600

    add bulma and add theming

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 // Components
 import { MergeMapComponent } from './merge-map/merge-map.component';
 import { ForkJoinComponent } from "./fork-join/fork-join.component";
+import { BulmaComponent } from "./bulma/bulma.component";
 
 const routes: Routes = [
   // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'merge-map', component: MergeMapComponent },
   { path: 'fork-join', component: ForkJoinComponent },
+  { path: 'bulma', component: BulmaComponent },
 ];
 
 @NgModule({
